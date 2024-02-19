3c3a324ad076c6ecbd43126d459ab7165920716f
rsgur <rafaelgs23@educastur.es>
2024-02-19 22:56:41 +0100

    Funciona, estamos incorporando el viewModelProviderFactory

--- app/src/main/java/com/mpd/pmdm/dicerollerconstraintlayout/Dice.kt
+++ app/src/main/java/com/mpd/pmdm/dicerollerconstraintlayout/Dice.kt
@@ -1,10 +1,24 @@
 package com.mpd.pmdm.dicerollerconstraintlayout
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 /**
  * Clase que modela un dado con un número de caras configurable
  */
 class Dice(private val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
+
+    //Guardamos en que estado se encuentra cada instancia del dado.
+    //para prevenir modificaciones indeseadas hacemos esto.
+    //propiedad Observable.
+    private val _currentSide = MutableLiveData<Int>(0)
+    val currentSide: LiveData<Int> = _currentSide
+
+    //Creamos una función adicional que inicialice al dado para dar un valor válido.
+    init {
+        roll()
+    }
+    fun roll() {
+        _currentSide.value = (1..numSides).random()
     }
 }
---
+++ app/src/main/java/com/mpd/pmdm/dicerollerconstraintlayout/TwoDicesViewModel.kt
@@ -0,0 +1,30 @@
+package com.mpd.pmdm.dicerollerconstraintlayout
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class TwoDicesViewModel(val numSides: Int): ViewModel() {
+
+    private val dice1 = Dice(6)
+    private val dice2 = Dice(6)
+
+    //podemos crear observadores de estas instancias.
+    val currentSideDice1: LiveData<Int> = dice1.currentSide
+    val currentSideDice2: LiveData<Int> = dice2.currentSide
+
+    fun rollDices(){
+        dice1.roll()
+        dice2.roll()
+    }
+}
+
+/**class TwoDicesViewModelFactory(val caras: Int): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>):T{
+        @Suppress("UNCHECKED_CAST")
+        if (modelClass.isAssignableFrom(TwoDicesViewModel::class.java))
+            return TwoDicesViewModel(caras) as T
+        throw IllegalArgumentException("ModelClass is not an instace of TwoDicesViewModel")
+    }
+
+}*/
